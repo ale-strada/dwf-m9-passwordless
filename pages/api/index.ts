32950f38b296ed9445681978828c05c10321d2e2
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-09-29 18:26:07 -0300

    probando conectar base de datos

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,18 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-//import { firestore } from "lib/firestore";
+import { firestore } from "lib/firestore";
 import { User } from "lib/user";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  // const newUSer = await firestore.collection("auth").add({
-  //   email: "ale@mail.com",
-  // });
+  const newUSer = await firestore.collection("auth").add({
+    email: "prueba@mail.com",
+  });
 
   // const user = await User.create({
   //   name: "ale",
   //   email: "ale@example.com",
   // });
 
-  const user = await User.findOne({ where: { id: "raHHtoWOUwHvF52m4pXn" } });
+  //const user = await User.findOne({ where: { id: "Rkn1wnmIhdIHHFkm4qIy" } });
 
-  res.send(user.data);
+  res.send(newUSer);
 }
