43f5f3d6b2c8cd6c1e25b7dc28134b2fbac5bc42
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-10-03 11:14:22 -0300

    prueba con libreria sequelize

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -4,18 +4,18 @@ import { User } from "lib/user";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   try {
-    const newUSer = await firestore.collection("auth").add({
-      email: req.body.email,
-    });
-
-    // const user = await User.create({
-    //   name: "ale",
-    //   email: "ale@example.com",
+    // const newUSer = await firestore.collection("auth").add({
+    //   email: req.body.email,
     // });
+
+    const user = await User.create({
+      name: "aleprueba",
+      email: "aleprueba@example.com",
+    });
 
     //const user = await User.findOne({ where: { id: "Rkn1wnmIhdIHHFkm4qIy" } });
 
-    res.send(newUSer);
+    res.send(user);
   } catch (error) {
     console.log(error);
     res.send(error);
