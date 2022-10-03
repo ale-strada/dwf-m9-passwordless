6c7a321a28cebbfa0c68f012afcb4af531ce5138
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-10-03 12:12:27 -0300

    buscando error en sequelize 5

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -3,15 +3,15 @@ import { firestore } from "lib/firestore";
 import { User } from "lib/user";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const newUSer = await firestore.collection("auth").add({
-    email: req.body.email,
-  });
+  // const newUSer = await firestore.collection("auth").add({
+  //   email: req.body.email,
+  // });
 
   // const user = await User.create({
   //   name: "aleprueba",
   //   email: "aleprueba@example.com",
   // });
 
-  //const user = await User.findOne({ where: { id: "Rkn1wnmIhdIHHFkm4qIy" } });
-  res.send(newUSer);
+  const user = await User.findOne({ where: { id: "Rkn1wnmIhdIHHFkm4qIy" } });
+  res.send(user);
 }
