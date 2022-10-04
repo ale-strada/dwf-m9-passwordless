1bed9a43339f0e89a0d42c4c41c74f607c9a9976
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-10-04 18:44:06 -0300

    ajustes en el metodo de auth/token

--- lib/sendgrid.ts
+++ lib/sendgrid.ts
@@ -1,17 +1,6 @@
 import sgMail from "@sendgrid/mail";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-//ES6
-// sgMail
-//   .send(msg)
-//   .then(() => {}, error => {
-//     console.error(error);
-
-//     if (error.response) {
-//       console.error(error.response.body)
-//     }
-//   });
-//ES8
 export async function sendEmailCode(msg) {
   try {
     await sgMail.send(msg);
--- lib/sequelize.ts
+++
@@ -1,6 +0,0 @@
-import { firestore, admin } from "./firestore";
-import firebaseSequelizer from "firestore-sequelizer";
-
-firebaseSequelizer.initializeApp(admin);
-
-export {};
